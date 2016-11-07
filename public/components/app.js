c5898a39aa47ca6f375e58c56653e25f638aaf9b
mr433 <max.rodewald@gmail.com>
2016-11-07 14:51:50 -0700

    Fixed urls

--- public/components/app.js
+++ public/components/app.js
@@ -4,42 +4,42 @@ angular.module('spotifyApp', ['ngCookies', 'ui.router'])
         //search state
         $stateProvider.state('recommendations', {
                 url: '/recommendations',
-                templateUrl: '/components/recommendations/recommendTmpl.html',
+                templateUrl: '/public/components/recommendations/recommendTmpl.html',
                 controller: 'recommendationController'
             })
 
             //login state
             .state('login', {
                 url: '/',
-                templateUrl: '/components/login/login.html',
+                templateUrl: '/public/components/login/login.html',
                 controller: 'loginController'
             })
 
             //artist pages
             .state('artist', {
                 url:'/artist/:id',
-                templateUrl: '/components/artist/artistTmpl.html',
+                templateUrl: '/public/components/artist/artistTmpl.html',
                 controller: 'artistController'
             })
 
             //album pages
             .state('album', {
                 url:'/album/:id',
-                templateUrl: '/components/album/albumTmpl.html',
+                templateUrl: '/public/components/album/albumTmpl.html',
                 controller:'albumController'
             })
 
             //search page
             .state('search', {
                 url:'/search',
-                templateUrl: '/components/search/searchTmpl.html',
+                templateUrl: '/public/components/search/searchTmpl.html',
                 controller: 'searchController'
             })
 
             //library page
             .state('library', {
                 url:'/library',
-                templateUrl:'/components/library/libraryTmpl.html',
+                templateUrl:'/public/components/library/libraryTmpl.html',
                 controller:'libraryController'
             });
 
